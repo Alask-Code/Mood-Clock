bf1c61a231bb30d9c8cba617089cadd31789e408
Alask-Code <anonimoplayer736@gmail.com>
2025-01-06 12:00:43 -0300

    implement web mode

--- src/app.js
+++ src/app.js
@@ -6,10 +6,25 @@ import randomPhrase from './scripts/randomPhrase.js';
 function App () {
   const domDate = document.querySelector('.date');
   initDate(domDate);
+
   const domClock = document.querySelector('.clock');
   initClock(domClock);
 
   const domPhrase = document.querySelector('.phrase');
   domPhrase.innerText = randomPhrase(badMoodPhrases);
+
+  domPhrase.onclick = () => {
+    domPhrase.classList.toggle('animate');
+    setTimeout(() => {
+      domPhrase.innerText = randomPhrase(badMoodPhrases);
+    }, 250);
+    setTimeout(() => {
+      domPhrase.classList.toggle('animate');
+    }, 500);
+  };
+  const isElectron = !!window.process;
+  if (!isElectron) {
+    document.body.style.background = 'rgb(58,58,58)';
+  };
 }
 App();
